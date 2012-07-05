d212eb887603146883d154d050da540395e2be4b
never2nv <fngnation@gmail.com>
2012-07-04 23:23:31 -0400

    Project 2 - Save 3
    Working on recipe categories drop box via javascript rather than html
    form inputs.

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,6 @@ Paul J. Miller
 VFW 1207 - Project 2
 07/03/2012
 */
-
-alert("Javascript Works");
-console.log("Test");
 	//Wait for DOM to be ready
 	window.addEventListener("DOMContentLoaded", function(){
 
@@ -16,9 +13,28 @@ console.log("Test");
 		return grabElement;	
 	}
 	
+	//Create select field element and populate with options.
+	function makeCats(){
+		var formTag = document.getElementsByTagName("form"), // formTag IS AN ARRAY OF ALL THE FORM TAGS!!!!!.
+			selectLi = $('select'),
+			makeSelect = document.createElement('select');
+			makeSelect.setAttribute("id", "groups");
+		
+		for (var i=0, j=recipeCategory.length; i<j; i++){
+			var makeOption = document.createElement('option');
+			var optText = recipeCategory[i];
+			makeOption.setAttribute("value", optText);
+			makeOption.innerHTML = optText;
+			makeSelect.appendChild("makeOption");
+		}
+		selectLi.appendChild(makeSelect);
+		
+	}
+	
 
 	//Variable defaults
 	var recipeCategory = ["--Choose A Recipe Category--", "Breakfast", "Lunch", "Dinner", "Desert", "Drink"];
+	makeCats();
 	
 
 	//Set Link & Submit Click Events
